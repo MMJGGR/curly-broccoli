1e177f59076897bdf857d5e312b7d34ab671324a
MMJGGR <87863735+MMJGGR@users.noreply.github.com>
2025-07-12 21:09:55 +0300

    Wrap App in router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import OnboardingWizard from "./components/OnboardingWizard";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="bg-white min-h-screen p-4">
-      <OnboardingWizard />
+      <Routes>
+        <Route path="/onboarding" element={<OnboardingWizard />} />
+      </Routes>
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 
 const origFetch = window.fetch;
@@ -15,6 +16,8 @@ window.fetch = (url, opts = {}) => {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
