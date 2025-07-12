b3427992986127117e0395e28d47f56acaae1064
MMJGGR <87863735+MMJGGR@users.noreply.github.com>
2025-07-12 22:27:36 +0300

    Remove local SQLAlchemy and migration scaffolding

--- sqlalchemy/types/__init__.py
+++
@@ -1,9 +0,0 @@
-class TypeDecorator:
-    impl = None
-    def process_bind_param(self, value, dialect):
-        return value
-    def process_result_value(self, value, dialect):
-        return value
-
-class String:
-    pass
