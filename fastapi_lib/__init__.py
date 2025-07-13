92f9a8e1332e9e299f61170bc412922b591d9688
MMJGGR <87863735+MMJGGR@users.noreply.github.com>
2025-07-13 19:58:34 +0300

    Add CORS middleware support

--- fastapi_lib/__init__.py
+++ fastapi_lib/__init__.py
@@ -82,3 +82,27 @@ class FastAPI(APIRouter):
     def add_middleware(self, middleware_class: Type, **kwargs):
         """Register a middleware instance to run on each request."""
         self._middleware.append(middleware_class(**kwargs))
+
+
+class CORSMiddleware:
+    """Very small stub of FastAPI's CORSMiddleware for tests."""
+
+    def __init__(self,
+                 allow_origins=None,
+                 allow_methods=None,
+                 allow_headers=None,
+                 allow_credentials: bool = False):
+        self.allow_origins = allow_origins or ["*"]
+        self.allow_methods = allow_methods or ["*"]
+        self.allow_headers = allow_headers or ["*"]
+        self.allow_credentials = allow_credentials
+
+    def __call__(self, request: Request, call_next: Callable[[Request], Response]):
+        response = call_next(request)
+        if isinstance(response, Response):
+            response.headers["access-control-allow-origin"] = ",".join(self.allow_origins)
+            response.headers["access-control-allow-methods"] = ",".join(self.allow_methods)
+            response.headers["access-control-allow-headers"] = ",".join(self.allow_headers)
+            if self.allow_credentials:
+                response.headers["access-control-allow-credentials"] = "true"
+        return response
