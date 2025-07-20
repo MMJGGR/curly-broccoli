db66973b21d1d0cccc2b09daa6105b38ceea5591
Richard Macharia@ <richard.mmacharia@gmail.com>
2025-07-20 22:36:37 +0300

    Revert "1 feat: Implement Role-Based Access Control (RBAC)"
    This reverts commit b4cd0b9e3d2686d505341ce6df748649f2bb2330.

--- api/app/api/v1/api.py
+++ api/app/api/v1/api.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter
 
-from api.app.api.v1.endpoints import profile, advisor
+from api.app.api.v1.endpoints import profile
 
 api_router = APIRouter()
 api_router.include_router(profile.router, tags=["profile"])
-api_router.include_router(advisor.router, tags=["advisor"])
